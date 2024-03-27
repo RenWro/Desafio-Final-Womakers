8a95036dffc2cb7496f872c6b2d5dc22e1ce6932
ldsleticia <lds.leticia.dos.santos@gmail.com>
2024-03-26 21:37:46 -0300

    feat: finalizando setup de banco de dados

---
+++ app/models.py
@@ -0,0 +1,49 @@
+from django.db import models
+
+
+class Editora(models.Model):
+    nome = models.CharField(max_length=150)
+    cnpj = models.IntegerField()
+
+
+class Genero(models.Model):
+    nome = models.CharField(max_length=150)
+
+
+class Usuario(models.Model):
+    nome = models.CharField(max_length=150)
+    sobrenome = models.CharField(max_length=150)
+    endereco = models.CharField(max_length=150)
+    email = models.CharField(max_length=150)
+    telefone = models.IntegerField()
+    cpf = models.IntegerField()
+    id_livros = models.ManyToManyField('Livro', blank=False)
+
+
+class Autores(models.Model):
+    nome = models.CharField(max_length=150)
+    sobrenome = models.CharField(max_length=150)
+
+
+class Pedidos(models.Model):
+    STATUS_CHOICES = [
+        ('PENDENTE', 'Pendente'),
+        ('EM_PROCESSAMENTO', 'Em Processamento'),
+        ('CONCLUIDO', 'Concluído'),
+        ('CANCELADO', 'Cancelado'),
+    ]
+    data = models.DateTimeField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES)
+    usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+
+class Livro(models.Model):
+    isbn = models.IntegerField()
+    titulo = models.CharField(max_length=150)
+    valor = models.FloatField()
+    estoque = models.IntegerField()
+    descricao = models.CharField(max_length=200)
+    editora = models.ForeignKey(Editora, on_delete=models.DO_NOTHING)
+    id_genero = models.ForeignKey(Genero, on_delete=models.DO_NOTHING)
+    autores = models.ManyToManyField(Autores)
+
