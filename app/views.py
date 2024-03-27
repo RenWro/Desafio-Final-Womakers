882cbd6cc16c5cf7786c25b5706bb328e124ad82
paula borges <paulabzambeli@gmail.com>
2024-03-26 23:50:57 -0300

    feat-listar-livros

--- app/views.py
+++ app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
+from app.models import Livro
 # Create your views here.
+
+def listar_livros(request):
+    livros = Livro.objects.all()
+    return render(request, 'listar_livros.html',{'livros': livros})
