0d59e61246db529acba2f9f9031a92001bfd9d84
paula borges <paulabzambeli@gmail.com>
2024-04-04 20:59:15 -0300

    login.html

--- livro/urls.py
+++ livro/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from app.views import listar_livros
+from . import views
 from django.http import HttpResponse
 
 app_name= 'Livros'
 urlpatterns = [
-    path('', listar_livros)
+    path('', views.listar_livros)
 ]
