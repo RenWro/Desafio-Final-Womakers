d268c8e9d180dc6305048b285b5860920cebdb8b
anapppp <anapaula.ksoares@gmail.com>
2024-04-03 21:53:36 -0300

    ap cliente: refatoracao do backend, alteracao do nome de funcoes e rotas

--- cliente/urls.py
+++ cliente/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from cliente.views import cadastrar_cliente, logout_cliente, verificar_cadastro, excluir_cadastro, consultar_perfil
+from cliente.views import cadastrar_cliente, logout_cliente, login_cliente, excluir_cadastro, consultar_perfil
 
 app_name = 'Cliente'
 urlpatterns = [
     path('cadastro/', cadastrar_cliente, name='cadastrar_cliente'),
     path('logout/', logout_cliente, name='logout_cliente'),
-    path('login/', verificar_cadastro, name='verificar_cadastro'),
-    path('excluir_cadastro/<int:usuario_id>', excluir_cadastro, name='excluir_cadastro'),
+    path('login/', login_cliente, name='login_cliente'),
     path('perfil/', consultar_perfil, name='consultar_perfil'),
+    path('excluir_cadastro/<int:cliente_id>',
+         excluir_cadastro, name='excluir_cadastro'),
 ]
