b195e35b32a5805c6b8352cfa4d372b1704a6c86
anapppp <anapaula.ksoares@gmail.com>
2024-04-05 21:39:24 -0300

    app livro: criacao de um template simples que mostra as informacoes dos livros; preenchimento de alguns campos da tabela livro_livro_autores

--- livro/urls.py
+++ livro/urls.py
@@ -3,9 +3,8 @@ from .views import listar_livros
 from . import views
 from django.http import HttpResponse
 
-app_name= 'Livros'
+app_name = 'Livros'
 
 urlpatterns = [
     path('', listar_livros),
-    path('', views.listar_livros),
 ]
